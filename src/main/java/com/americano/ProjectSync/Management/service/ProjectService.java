41651616fd2b73eef76214ec63e250801b8543b0
xXCpFireXx <suarezalejo26@gmail.com>
2025-10-23 18:23:17 -0500

    feat: finished backend of CREATE project

--- src/main/java/com/americano/ProjectSync/Management/service/ProjectService.java
+++ src/main/java/com/americano/ProjectSync/Management/service/ProjectService.java
@@ -1,16 +1,22 @@
 package com.americano.ProjectSync.Management.service;
 
+import com.americano.ProjectSync.Management.model.Project;
 import com.americano.ProjectSync.Management.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProjectService {
-    private ProjectRepository projectRepository;
+    private final ProjectRepository projectRepository;
 
     @Autowired
     public ProjectService(ProjectRepository projectRepository) {
         this.projectRepository = projectRepository;
     }
+
+    public Project createProject(Project project){
+        return projectRepository.save(project);
+    }
+
 
 }
---
+++ src/main/resources/static/js/app-create.js
@@ -0,0 +1,71 @@
+const projectForm = document.getElementById("projectForm");
+const title = document.getElementById("titleProject");
+const description = document.getElementById("descriptionProject");
+const responsible = document.getElementById("responsibleProject");
+
+const API_URL = "http://localhost:8080";
+
+projectForm.addEventListener("submit", async (event) => {
+  event.preventDefault(); 
+  
+  try {
+    const newProjectData = {
+      title: capitalizeEachWord(title.value),
+      description: description.value,
+      status: "Active",
+      responsiblePerson: capitalizeEachWord(responsible.value),
+    };
+
+    const response = await fetch(`${API_URL}/api/projects`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newProjectData),
+    });
+
+    if (!response.ok) {
+      notification("An error occurred.", "#b60404ff", 3000);
+      const errorData = await response.json().catch(() => ({}));
+      console.error("Error details:", errorData);
+      throw new Error(errorData.message || "An error occurred.");
+    }
+
+    notification("Successful!", "#a7c957", 3000);
+    resetForm();
+    
+    setTimeout(() => {
+        window.location.href = "./index.html";
+    }, 1000);
+
+  } catch (error) {
+    console.error("Error creating project:", error);
+    notification("Error creating project", "#b60404ff", 3000);
+  }
+});
+
+function resetForm() {
+  projectForm.reset(); 
+}
+
+const capitalizeEachWord = (str) => {
+  if (!str) return '';
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+};
+
+const notification = (text, color, duration = 3000) => {
+  Toastify({
+    text,
+    duration,
+    close: true,
+    gravity: "top", 
+    position: "center", 
+    stopOnFocus: true, 
+    style: {
+      borderRadius: "8px",
+      padding: "15px",
+      background: color,
+    },
+  }).showToast();
+};
