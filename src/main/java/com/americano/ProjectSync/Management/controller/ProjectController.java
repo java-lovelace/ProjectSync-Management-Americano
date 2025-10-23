3135cc51bac5ee5bde781fb297bbe4946084dafc
xXCpFireXx <suarezalejo26@gmail.com>
2025-10-23 11:14:49 -0500

    add new changes in controller and service

--- src/main/java/com/americano/ProjectSync/Management/controller/ProjectController.java
+++ src/main/java/com/americano/ProjectSync/Management/controller/ProjectController.java
@@ -1,4 +1,19 @@
 package com.americano.ProjectSync.Management.controller;
 
+import com.americano.ProjectSync.Management.service.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/projects")
 public class ProjectController {
+    private final ProjectService projectService;
+
+    @Autowired
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+
 }
--- src/main/java/com/americano/ProjectSync/Management/service/ProjectService.java
+++ src/main/java/com/americano/ProjectSync/Management/service/ProjectService.java
@@ -1,4 +1,16 @@
 package com.americano.ProjectSync.Management.service;
 
+import com.americano.ProjectSync.Management.repository.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProjectService {
+    private ProjectRepository projectRepository;
+
+    @Autowired
+    public ProjectService(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
 }
