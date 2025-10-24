67ecca3c969f2f42755106d0d3ae31ec192ee60f
SamuelZ7021 <samuelandrey1207@gmail.com>
2025-10-23 19:31:11 -0500

    Project Deletion Method and Error Handling in service/ProjectService, controller/ProjectController and exception/ResourcesNotFoundException

--- src/main/java/com/americano/ProjectSync/Management/controller/ProjectController.java
+++ src/main/java/com/americano/ProjectSync/Management/controller/ProjectController.java
@@ -4,6 +4,8 @@ import com.americano.ProjectSync.Management.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/projects")
@@ -13,6 +15,15 @@ public class ProjectController {
     @Autowired
     public ProjectController(ProjectService projectService) {
         this.projectService = projectService;
+    }
+
+    // We use key notation to do the elimination method
+    // DELETE /api/projects/{id}
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProject(@PathVariable Long id){
+        projectService.deleteProject(id);
+
+        return ResponseEntity.noContent().build();
     }
 
 
--- src/main/java/com/americano/ProjectSync/Management/service/ProjectService.java
+++ src/main/java/com/americano/ProjectSync/Management/service/ProjectService.java
@@ -3,6 +3,8 @@ package com.americano.ProjectSync.Management.service;
 import com.americano.ProjectSync.Management.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.americano.ProjectSync.Management.exception.ResourcesNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ProjectService {
@@ -13,4 +15,17 @@ public class ProjectService {
         this.projectRepository = projectRepository;
     }
 
+    // Logic for CRUD operations DELETE by ID
+    @Transactional
+    public void deleteProject(Long id){
+        // We check if the project exists before deleting it
+        boolean exists = projectRepository.existsById(id);
+
+        // If the project does not exist, it enters the conditional to launch the NotFound
+        if(!exists){
+            throw new ResourcesNotFoundException("Project with id " + id + " does not exists");
+        }
+        // The delete operation continues and the project is deleted
+        projectRepository.deleteById(id);
+    }
 }
